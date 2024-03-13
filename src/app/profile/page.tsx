8b70f01d3e5dda83a41c050a0fbe67327705c975
Ahmadtito17 <ahmadtitohendrawan@gmail.com>
2024-03-13 12:37:36 +0700

    Praktikum 1

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+export default function Profile() {
+    return (
+      <div>
+        <h1>Welcome to My Profile</h1>
+        <p>Nama: Ahmad Tito Nur Hendrawan</p>
+        <p>NIM: 2141720265</p>
+        <p>Institusi: Politeknik Negeri Malang</p>
+        <p>Jurusan: Teknologi Informasi</p>
+        <p>Program Studi: D-IV Teknik Informatika</p>
+      </div>
+    );
+  }
